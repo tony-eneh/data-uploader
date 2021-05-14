5defa45ff7ab360144a144bc8dc49a965bf48d3a
Tony Eneh <aeneh90@gmail.com>
2021-05-14 16:58:52 +0100

    Feat: implement /student-classes endpoint

--- index.js
+++ index.js
@@ -10,6 +10,62 @@ require("dotenv").config();
 app.use(fileUpload());
 app.use(bodyParser.json());
 
+app.put("/student-classes", async (req, res) => {
+  console.log("about to update student classes");
+  const successfulUpdates = [];
+  const errors = [];
+
+  const institutions = ["CRECHE", "NURSERY", "PRIMARY", "SECONDARY"];
+  const levels = [1, 2, 3, 4, 5, 6];
+
+  for (let institution of institutions) {
+    for (let level of levels) {
+      switch (institution) {
+        case "CRECHE":
+          //  no CRECHE 2 class
+          if (level > 1) return;
+        case "NURSERY":
+          // no NURSERY 4 class
+          if (level > 3) return;
+      }
+
+      // fetch a classe
+      const classeResponse = await axios.get(
+        `${process.env.API_URL}/${classe}?level=${level}&institution=${institution}&subclass=R&fields=id`
+      );
+      const classe = classeResponse.payload[0];
+
+      // fetch students that belong to that classe
+      const studentResponse = await axios.get(
+        `${process.env.API_URL}/${student}?level=${level}&institution=${institution}&fields=id`
+      );
+      const students = studentResponse.payload;
+
+      // assign the classe id to the each of the student's classe property
+      const classedStudents = students.map(
+        (student) => (student.classe = classe.id)
+      );
+
+      // send an update request to the API
+      try {
+        const apiResponse = await axios.put(
+          `${process.env.API_URL}/student/batch`,
+          classedStudents
+        );
+        successfulUpdates.push(apiResponse);
+      } catch (err) {
+        errors.push(err);
+      }
+    }
+  }
+  res.send({
+    success: !!successfulUpdates.length, // mark successful if at least 1 update succeeded
+    message: `successfully updated ${successfulUpdates.length} records. ${errors.length} requests failed`,
+    payload: [...successfulUpdates],
+    errors: [...errors],
+  });
+});
+
 app.post("/:endpoint/csv", (req, res) => {
   console.log("request");
   const results = [];
